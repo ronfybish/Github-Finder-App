7946f0d4da40317b702c8f17477d49d881798dc9
Ron Fybish <41908747+ronfybish@users.noreply.github.com>
2020-05-12 10:00:53 +0300

    set up connection and routing for server

--- client/src/components/users/User.js
+++ client/src/components/users/User.js
@@ -62,7 +62,7 @@ const User = ({ match }) => {
               <p>{bio}</p>
             </Fragment>
           )}
-          <a href={html_url} className='btn btn-dark my-1'>
+          <a href={html_url} target="_blank" rel="noopener noreferrer" className='btn btn-dark my-1'>
             Visit Github Profile
           </a>
           <ul>
