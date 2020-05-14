0b5bba13fdb8800b4215ad39b57700c62d1adb9a
Ron Fybish <41908747+ronfybish@users.noreply.github.com>
2020-05-14 14:38:41 +0300

    make it better

--- client/src/context/producthunt/ProducthuntState.js
+++ client/src/context/producthunt/ProducthuntState.js
@@ -23,12 +23,22 @@ const Producthunt = props => {
   //GET Products
   const getProducts=async (topic)=>{
     try {
-
+      const token='gje-a-bn0bDzNsYEtD4l2qh7D4J62TYr5jlK7HqMtUE'
+      const config={
+        headers:{
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`,
+            'Host': 'api.producthunt.com'
+        }
+    }
       setLoading()
-      const res= await axios.get(`/api/producthunt/${topic}`)
+      // const res= await axios.get(`/api/producthunt/${topic}`)
+      const res= await axios.get(`https://api.producthunt.com/v1/posts/all?search[topic]=${topic}`,config);
+      console.log(res.data.posts)
       dispatch({
           type:GET_PRODUCTS,
-          payload:res.data
+          payload:res.data.posts
       })
       
     } catch (error) {
@@ -40,9 +50,18 @@ const Producthunt = props => {
   // Get Product
   const getProduct = async id => {
     try {
-
+      const token='gje-a-bn0bDzNsYEtD4l2qh7D4J62TYr5jlK7HqMtUE'
+      const config={
+        headers:{
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`,
+            'Host': 'api.producthunt.com'
+        }
+    }
       setLoading();
-      const res= await axios.get(`/api/producthunt/tool/${id}`)
+      // const res= await axios.get(`/api/producthunt/tool/${id}`)
+      const res= await axios.get(`https://api.producthunt.com/v1/posts/${id}`,config);
       console.log(res.data)
       dispatch({
         type: GET_PRODUCT,
